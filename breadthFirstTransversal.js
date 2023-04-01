60eeb4bcd67430749ff84ada873a04fcd7e9db49
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-04-01 13:43:33 +0545

    added breadthFirstTransversal

---
+++ breadthFirstTransversal.js
@@ -0,0 +1,36 @@
+const adjList = {
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5, 6],
+  5: [1, 2, 4],
+  6: [4],
+};
+
+function printBreadthFirst(start) {
+  // your code here
+  let current = start;
+  let queue = [current];
+  let visited = new Set();
+  visited.add(start);
+  while (queue.length > 0) {
+    let node = queue.shift();
+    console.log(node);
+    for (let item of adjList[node]) {
+      if (!visited.has(item)) {
+        visited.add(item);
+        queue.push(item);
+      }
+    }
+  }
+}
+
+console.log("First Test:");
+printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
+// One possible solution:  3, 2, 4, 1, 5, 6
+console.log("Second Test:");
+printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+// One possible solution:  6, 4, 3, 5, 2, 1
+console.log("Third Test:");
+printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+// One possible solution:  4, 3, 5, 6, 2, 1
